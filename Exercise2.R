9103651b093bacdf399f7255f21abad03111b73a
Anthony <ajbuchanjr@gmail.com>
2016-06-22 23:31:56 -0400

    Update Exercise2.R

--- Exercise2.R
+++ Exercise2.R
@@ -1,9 +1,16 @@
+# Author: Anthony J Buchanan
+# File Description: Reads in data on the Titanic and replaces null data 
+# with standard values
 library(dplyr)
 library(tidyr)
 # setwd("D:/Users/Anthony/Documents/Springboard")
-tbl <- read.csv("titanic_original.csv", stringsAsFactors=F, na.strings=c("","NA"))
-refinetbl <- replace_na(tbl, list(embarked = "S", age = mean(tbl$age, na.rm = T), boat = "None"))
-# Missing cabin numberlikely means there was no cabin assigned as the passanger was likely crew or in steerage
-refinetbl <- mutate(refinetbl, has_cabin_number = ifelse(is.na(cabin),0,1))
-View(refinetbl)
-write.csv(refinetbl, "titanic_clean.csv")
+table.load <- read.csv("titanic_original.csv", stringsAsFactors=F, 
+  na.strings=c("","NA"))
+refined.table <- replace_na(table.load , list(embarked = "S", age = 
+  mean(table.load$age, na.rm = T), boat = "None"))
+# Missing cabin number likely means there was no cabin assigned as the 
+# passenger was likely crew or in steerage
+refined.table <- mutate(refined.table, has_cabin_number = 
+  ifelse(is.na(cabin),0,1))
+View(refined.table)
+write.csv(refined.table, "titanic_clean.csv")
