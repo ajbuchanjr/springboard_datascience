fa7d631bfc64b993b28e214095a8b3ca75342c6c
Anthony <ajbuchanjr@gmail.com>
2016-06-27 22:03:50 -0400

    Added Database connection

--- reddit_scraper.R
+++ reddit_scraper.R
@@ -7,7 +7,8 @@ library(dplyr)
 library(R.utils)
 library(httr)
 library(httpuv)
-# setwd("D:/Users/Anthony/Documents/Springboard")
+library(DBI)
+setwd("D:/Users/Anthony/Documents/Springboard")
 RedditToken <- function(appscript, secretvalue) {
   # Loads Reddit OAuth2 tokens.
   #
@@ -24,11 +25,11 @@ RedditToken <- function(appscript, secretvalue) {
     access = "https://www.reddit.com/api/v1/access_token")
   app <- oauth_app("reddit", appscript, secretvalue)
   token <- oauth2.0_token(reddit, app, scope = c("read"), 
-    use_basic_auth = TRUE, user_agent("rdatapull2 v0.2 by rdata"))
+    use_basic_auth = TRUE, user_agent("rpulldata v0.3 by u/rdata"))
   return(token)
 }
 # Get the Reddit Token
-my.token <- RedditToken("gOfXlYcHrvrWTg", "secretvalue")
+my.token <- RedditToken("gOfXlYcHrvrWTg", "0aAflGdXeKPiaDcq_uaPeFBa1oo")
 LoadData <- function(url.val) {
   # Loads comment data from a given reddit url.
   #
@@ -39,47 +40,60 @@ LoadData <- function(url.val) {
   #  A data fame with comments, including subreddit, created date
   #  author, score (total, up and down  votes), and comment text
   init.req <- GET(url.val, config(token = my.token), 
-    user_agent("rdatapull1 v0.2 by rdata"))
+    user_agent("rpulldata v0.3 by u/rdata"))
   init.req <- content(init.req, as = "text")
   main.data <- tryCatch({
     fromJSON(init.req)
     if(!is.atomic(main.data[[2]]))
     {
-      before.link <- main.data[[2]]$before
-      after.link <- main.data[[2]]$after
       main <- main.data[[2]]$children$data
+      main["before"]<- main.data[[2]]$before
+      main["after"] <- main.data[[2]]$after
+      main["inserteddate"] <- Sys.time()
     }
   }, error = function(e) {
     cat("ERROR :",conditionMessage(e), "\n")
     Sys.sleep(600)
     })
   
-  reduced.main <- tryCatch({ main[, c("subreddit","created","author","score",
-                             "downs","ups","body")]
+  reduced.main <- tryCatch({ main[, c("id","subreddit","created","author","score",
+                             "downs","ups","body","after","inserteddate")]
   }, error = function(e) {
     cat("ERROR :",conditionMessage(e), "\n")
-    reduced.main <- data.frame(subreddit = character(0), created = numeric(0), 
-     author = character(0), score = integer(0), downs = integer(0), 
-     ups = integer(0), body = character(0), after.link = character(0))
+    reduced.main <- data.frame(id = character(0), subreddit = character(0), 
+    created = numeric(0), author = character(0), score = integer(0), 
+    downs = integer(0), ups = integer(0), body = character(0), 
+    after = character(0))
   })
-  if(!is.atomic(reduced.main) && exists(after.link))
-  {  
-    reduced.main$after.link <- after.link
-  }
     return(reduced.main)
 }
 subreddit <- "worldnews"
 url.start <- paste("https://oauth.reddit.com/r/",subreddit,
-                   "/comments/.json?&t=year",sep="")
-total.data <- LoadData(url.start)
-if(!"after.link" %in% colnames(total.data))
+                     "/comments/.json?t=year",sep="")
+# Database Stuff
+devdb <- dbConnect(RSQLServer::SQLServer(), server="localhost", port=1433,
+                   properties=list(user="rdata", password="password"))
+commentstart <- dbGetQuery(devdb, "select top 1 id from Comments order by inserteddate DESC")
+if(nrow(commentstart) > 0)
 {
-  total.data[,"after.link"] <- NA
+  url.next <- paste(url.start,"&after=", gsub("t1_","",commentstart[1,1]), sep="")
+} else {
+  url.next <- url.start
 }
-  while (!is.null(tail(total.data["after.link"],1))) {
-    url.next <- paste(url.start,"&after=", tail(total.data["after.link"],1), sep="")
-    total.data <- rbind_list(total.data, LoadData(url.next))
-    print(nrow(total.data))
+total.data <- LoadData(url.next)
+dbWriteTable(devdb, name="Comments", total.data, append = T, overwrite = F)
+while (nrow(tail(total.data["after"],1)) > 0) {
+    url.next <- paste(url.start,"&after=", gsub("t1_","",tail(total.data["after"],1)), sep="")
+    total.data <- LoadData(url.next)
+    idcheck <- dbGetQuery(devdb, paste("select id from Comments where id in (", paste("'",total.data$id,collapse="',",sep=""), "')",sep=""))
+    total.data <- anti_join(total.data, idcheck, by="id")
+    if (nrow(total.data) < 0)
+    {
+      dbWriteTable(devdb, name="Comments", total.data, append = T, overwrite = F)
+    }
+    # total.data <- rbind_list(total.data, LoadData(url.next))
+    # print(nrow(total.data))
   }
-write.csv(total.data, paste("RedditYear",subreddit,"Comments.csv",sep=""))
-View(total.data)
+# write.csv(total.data, paste("RedditYear",subreddit,"Comments.csv",sep=""))
+# View(total.data)
+dbDisconnect(devdb)
