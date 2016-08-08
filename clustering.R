628a8881135ec262baf23f3257b5ad67cec7323a
Anthony <ajbuchanjr@gmail.com>
2016-08-08 00:08:36 -0400

    Update clustering.R

--- clustering.R
+++ clustering.R
@@ -1,3 +1,5 @@
+# Author: Anthony J Buchanan
+# File Description: Clustering Homework
 # This mini-project is based on the K-Means exercise from 'R in Action'
 # Go here for the original blog post and solutions
 # http://www.r-bloggers.com/k-means-clustering-from-r-in-action/
